04519566cec917d1cc52d5f806589be781ac57f1
creative-buffer-9578 <cmuusha@gmail.com>
2020-07-05 05:48:54 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = new Object({FrankOcean: 'Chanel'})
 function updatePlaylist(playlist, artist, song) {
   playlist[artist] = song
 }
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist.artist
+}
